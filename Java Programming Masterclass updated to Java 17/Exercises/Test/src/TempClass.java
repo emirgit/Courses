7dc87d24b8880f8261894268705c2eb5e67417f3
emirgit <emirtr852816@hotmail.com>
2023-12-17 20:40:27 +0300

    Day20
    Day20

---
+++ Java Programming Masterclass updated to Java 17/Exercises/Test/src/TempClass.java
@@ -0,0 +1,27 @@
+class TempClass {
+
+    int i;
+
+    public TempClass(int i) {
+        this.i = i;
+    }
+
+    public void TempClass(int j) {
+
+        int i = j;
+
+    }
+
+}
+
+
+
+public class C {
+
+    public static void main(String[] args) {
+
+        TempClass temp = new TempClass(2);
+
+    }
+
+}
